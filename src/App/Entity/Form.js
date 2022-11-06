d4e41d6d93f51d94adfde4a4b6883958fba776ba
Rita Carrilho <76398242+ritacarrilho@users.noreply.github.com>
2022-11-06 11:15:01 +0100

    Update Form.js

--- src/App/Entity/Form.js
+++ src/App/Entity/Form.js
@@ -1,10 +1,4 @@
 export class Form {
-    /**
-     * <input type=”text”>
-     * <textarea>
-     * <input type=”datetime-locasl”>
-     * <input type=”number”>
-     */ 
     title = null;
     description = null;
     beginDate = null;
@@ -14,45 +8,9 @@ export class Form {
 
     submitBtn = null;
 
-    constructor() {
-      // this.title = document.querySelector('#title');
-      // this.description = document.querySelector('#description');
-      // this.lat = document.querySelector('#lat');
-      // this.lon = document.querySelector('#lon');
-      // // console.log(this.endDate);
-      // this.submitBtn = document.querySelector('#submit');
-    }
+    constructor() {}
 
     getForm() {
-          //   <form  class="m-3">
-          //   <div class="form-group">
-          //     <label for="title">Event Name</label>s
-          //     <input type="text" class="form-control" id="title" aria-describedby="emailHelp">
-          //   </div>
-          //   <div class="form-group">
-          //     <label for="description">Description</label>
-          //     <textarea class="form-control" id="description" rows="3"></textarea>
-          //   </div>
-          //   <div class="form-group">
-          //     <label for="begin-date">Begin Date</label>
-          //     <input type="date" class="form-control" id="begin-date">
-          //   </div>
-          //   <div class="form-group">
-          //     <label for="end-date">End Date</label>
-          //     <input type="date" class="form-control" id="end-date">
-          //   </div>
-          //   <div class="form-group">
-          //     <label for="lat">Latitude</label>
-          //     <input type="number" class="form-control" id="lat">
-          //   </div>
-          //   <div class="form-group">
-          //     <label for="lon">Longitude</label>
-          //     <input type="number" class="form-control" id="lon">
-          //   </div>
-            
-          //   <button type="submit" class="btn btn-primary mt-3">Create</button>
-          // </form>
-
         let sidePanel = document.querySelector('.side-panel');
         let form = document.createElement('form');
 
@@ -84,7 +42,6 @@ export class Form {
             
             <button type="submit" id="submit" class="btn btn-dark mt-3">Create</button>`;
 
-            //form.classList.add('m-3'); give padding to form 
             sidePanel.appendChild(form);
             this.beginDate = document.querySelector('#begin');
             this.endDate = document.querySelector('#end');
@@ -92,8 +49,6 @@ export class Form {
             this.description = document.querySelector('#description');
             this.lat = document.querySelector('#lat');
             this.lon = document.querySelector('#lon');
-
-            // console.log(sidePanel);
     }
 
     clearInputs() {
